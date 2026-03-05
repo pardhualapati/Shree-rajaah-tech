b8dd5b855ecaa47aaf6445691a34c37e9e268efe
Pardhu Alapati <80155814+pardhualapati@users.noreply.github.com>
2026-03-04 20:41:35 -0600

    Update app.js

--- app.js
+++ app.js
@@ -37,10 +37,15 @@ app.post("/contact", async (req, res) => {
 
     try {
         let transporter = nodemailer.createTransport({
-            service: "gmail",
+            host: "smtp.gmail.com",
+            port: 587,
+            secure: false,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
+            },
+            tls: {
+                rejectUnauthorized: false
             }
         });
 
